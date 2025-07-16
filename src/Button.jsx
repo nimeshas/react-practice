6ba63a5c3437e59712399889b9e2265bc888d464
nimesha <nimesha.s106@gmail.com>
2025-07-17 00:54:10 +0530

    click event

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,3 +1,4 @@
+//click event here
 function Button(){
     
         //inline css styles to avoid conflicts with external stylesheets
@@ -11,9 +12,18 @@ function Button(){
             fontFamily: "Arial, Helvetica, sans-serif",
             margin:"0",
         }
-        return(<button style={styles}>
-            Click It :)
-        </button> );
+        let count=0;
+        const handleClick=(name)=>{
+            if(count<3){
+                count++;
+                console.log(`${name} clicked me ${count} time(s)`);
+            }
+            else{
+                console.log(` ${name} stop clicking omg!!`);
+            }
+
+        }
+        return(<button onClick={()=>handleClick("vro")} style={styles}>Click It :)</button> );
 
 }
-export default Button
+export default Button 
